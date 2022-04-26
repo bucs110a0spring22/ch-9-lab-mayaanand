1d8be4c5d2d01c93dc398c1b6d9f19ba82ff4c95
Maya E E <98714433+mayaanand@users.noreply.github.com>
2022-04-26 01:47:53 +0000

    finall

--- Rectangle.py
+++ Rectangle.py
@@ -1,9 +1,11 @@
 class Rectangle:
   def __init__(self, x, y, h, w):
+    #takes x, y, h, w as parameter and save them as instance variables
     self.x=max(0, x)
     self.y=max(0, y)
     self.height=max(0, h)
     self.width=max(0, w)
   def __str__(self):
+    #s is a string which contains the x, y, width and height of the rectangle
     s="(x: {}, y: {}) width: {}, height: {}"
     return s.format(self.x, self.y, self.width, self.height)
--- Surface.py
+++ Surface.py
@@ -1,7 +1,9 @@
 from Rectangle import Rectangle
 class Surface:
   def __init__(self, filename, x, y, h, w):
+    #it takes filename, x, y, h, w as a parameter and saves them as instance variables
     self.image=filename
     self.rect=Rectangle(x, y, h, w)
   def getRect(self):
+    #getRect is a method which returns rectangle object 
     return self.rect
